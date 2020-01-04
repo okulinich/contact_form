9cc3f6b58ef3c2e4acb0aec383b8ddf58a7a2f6e
okulinich <sashakulinich11@gmail.com>
2020-01-05 01:39:40 +0200

    full functionality

--- index.php
+++ index.php
@@ -12,10 +12,14 @@
             <form action="" method="post">
                 <input class="area" type="email" name="quest_email" placeholder="Your email" maxlength="32" required><br>
                 <textarea class="area" name="message" placeholder="message" row="8" required></textarea><br>
-                <input id="send_button" type="submit" value="Send"><br>
+                <input id="send_button" name="submit" type="submit" value="Send"><br>
             </form>
         </div>
 
-        
+        <?php
+            // if(isset($_POST['submit'])) {
+                mail("sashakulinich11@gmail.com", "From contact form " . $_POST['quest_email'], $_POST['message']);
+            //}
+        ?>
     </body>
 </html>
